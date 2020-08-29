f8cde3ffed0cfde3f07ce62f12ec11c0d362e177
suchenguo <guozhenduo1@163.com>
2020-08-29 20:07:18 +0800

    Update interpreter.py

--- interpret.py
+++ interpret.py
@@ -6,12 +6,26 @@ file_name = sys.argv[1]
 file_obj = open(f"{file_name}","r") 
 inline = 1 # Tell the program, what line is it
 file = file_obj.readlines()
+j = 0
 
 for i in file:
     statement = " ".join((i.split("#")[0]).split())
     
-    if statement[-1] != ';':
+    if statement[-1] != ';' and not(statement[-1] == "\\"):
+        # report error 
         print(f"In {file_name}:{inline}:error: \nexpected ';' after statment.")
         print(f"    {statement}")
-        
-    print(f"{statement.replace(';','')}")
+        print(" " * len(statement) + 3 * " ","^")
+
+        exit(123)
+
+    elif j == 1:
+        continue
+
+    elif statement[-1] == '\\':
+        print(f"{statement.replace(';','')[:-1]}")
+        j = 1
+        continue
+
+    else:
+        print(f"{statement.replace(';','')}")
